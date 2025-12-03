a981dfe00cda2c807c148fc0a6a713bab65447ad
Aidrocker <aydarmaximov@yandex.ru>
2025-12-03 09:29:09 +0300

    aidrocker: Added burger menu

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="app__container">
     <nav class="app__navbar">
-      <router-link to="/about" class="app__navbar-link">
-        О себе
-      </router-link>
-      <router-link to="/skills" class="app__navbar-link">
-        Навыки
-      </router-link>
-      <router-link to="/experience" class="app__navbar-link">
-        Опыт работы
-      </router-link>
-      <!-- <router-link to="/projects" class="app__navbar-link">Pet projects</router-link> -->
-      <router-link to="/contacts" class="app__navbar-link">
-        Контакты
-      </router-link>
+      <div class="app__navbar-menu-icon" @click="toggleMenu" v-if="isMobile">
+        <span :class="['app__navbar-menu-line', {'app__navbar-menu-line--open': menuOpen}]"></span>
+        <span :class="['app__navbar-menu-line', {'app__navbar-menu-line--open': menuOpen}]"></span>
+        <span :class="['app__navbar-menu-line', {'app__navbar-menu-line--open': menuOpen}]"></span>
+      </div>
+      
+      <div :class="['app__navbar-links', {'app__navbar-links--open': menuOpen}]">
+        <router-link to="/about" class="app__navbar-link" @click="closeMenu">
+          О себе
+        </router-link>
+        <router-link to="/skills" class="app__navbar-link" @click="closeMenu">
+          Навыки
+        </router-link>
+        <router-link to="/experience" class="app__navbar-link" @click="closeMenu">
+          Опыт работы
+        </router-link>
+        <!-- <router-link to="/projects" class="app__navbar-link">Pet projects</router-link> -->
+        <router-link to="/contacts" class="app__navbar-link" @click="closeMenu">
+          Контакты
+        </router-link>
+      </div>
     </nav>
     <main class="app__main-content">
       <transition name="fade" mode="out-in">
@@ -24,7 +32,35 @@
 </template>
 
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
 defineOptions({
   name: 'App'
 })
+
+const menuOpen = ref(false)
+const isMobile = ref(false)
+const resizeHandler = () => {
+  isMobile.value = window.innerWidth <= 768
+  if (!isMobile.value) {
+    menuOpen.value = false
+  }
+}
+
+onMounted(() => {
+  resizeHandler()
+  window.addEventListener('resize', resizeHandler)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', resizeHandler)
+})
+
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value
+}
+
+const closeMenu = () => {
+  menuOpen.value = false
+}
 </script>
